0a4d68a78d06490a3f8f1991e8c29d875be12ee9
J1nu-Lo3 <jcezar61@yahoo.com>
2026-01-16 16:05:17 +0100

    leger modif ajout pour page error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,14 @@
 import "../Styles/Error.scss";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div className="error">
+    <main className="error">
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
 
-      <a href="/">Retourner sur la page d’accueil</a>
-    </div>
+      <Link to="/">Retourner sur la page d’accueil</Link>
+    </main>
   );
 }
 export default Error;
