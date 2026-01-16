7d489466dd9854748a76898414cc465ffda3baa5
J1nu-Lo3 <jcezar61@yahoo.com>
2026-01-16 15:15:08 +0100

    leger modif cards

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -12,13 +12,11 @@ function Gallery() {
           className="card-link"
         >
           <article key={logement.id} className="card">
-            <div className="lmj-gallery">
-              <img
-                src={logement.cover}
-                alt={logement.title}
-                className="card__image"
-              />
-            </div>
+            <img
+              src={logement.cover}
+              alt={logement.title}
+              className="card__image"
+            />
             <h2 className="card__title">{logement.title}</h2>
           </article>
         </Link>
