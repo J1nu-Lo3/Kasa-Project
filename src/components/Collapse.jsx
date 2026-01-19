2e9bde4cb41420b0fdf51fedc13e2aa3e623f8e9
J1nu-Lo3 <jcezar61@yahoo.com>
2026-01-19 17:01:39 +0100

    ajout traveaux sur page A propos avec les collapses

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "../components/Collapse.scss";
+import arrow from "../assets/arrow.png";
+
+function Collapse({ title, content }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={`collapse ${open ? "is-open" : ""}`}>
+      <button
+        className="collapse__header"
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <span>{title}</span>
+
+        <img
+          src={arrow}
+          alt="toggle"
+          className={`collapse__arrow ${open ? "rotate" : ""}`}
+        />
+      </button>
+
+      <div className="collapse__content">
+        <div className="collapse__text">
+          <p>{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,20 @@
 import BannerA from "../components/BannerAbout";
+import Collapse from "../components/Collapse";
+import { aboutList } from "../data/aboutList";
 import "../Styles/About.scss";
 
 function About() {
   return (
     <main className="about">
       <BannerA />
+
+      <section className="about__list">
+        {aboutList.map((item, index) => (
+          <Collapse key={index} title={item.title} content={item.content} />
+        ))}
+      </section>
     </main>
   );
 }
+
 export default About;
