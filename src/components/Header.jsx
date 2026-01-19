9c8e88f9e2f388e09948e51b12fe8a07c1fcae3c
J1nu-Lo3 <jcezar61@yahoo.com>
2026-01-19 17:53:18 +0100

    leger modif sur header, gallery et collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import "../components/Header.scss";
 import logo from "../assets/LOGO.png";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
       <img src={logo} alt="Kasa" className="header__logo" />
       <nav className="nav-links">
-        <a href="/">Accueil</a>
-        <a href="/about">A propos</a>
+        <NavLink to="/" end>
+          Accueil
+        </NavLink>
+        <NavLink to="/about">A propos</NavLink>
       </nav>
     </header>
   );
